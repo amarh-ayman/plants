a2f3d59bc82bbe96bacc201c1fa3763b38829120
Amarah Abusohyon <amarhbishtawi@gmail.com>
2021-02-23 16:26:06 +0200

    my first javascript

---
+++ class06_JV/actions.js
@@ -0,0 +1,14 @@
+
+alert("HELLO IN OUR SOLAR SYSTEM , WELLCOME YOU !! ");
+
+var page_color =prompt (" what is your favourite color for the page ,HUMAN ? \n ENTER WHAT YOU WANT");
+var txt_color=prompt (" what is your favourite color for the text ,HUMAN ?\n ENTER WHAT YOU WANT");
+
+var y =document.getElementById('back_ground_color');
+y.style.background=page_color;
+
+var x=document.getElementById('text_color');
+x.style.color=txt_color;
+
+
+confirm("BY READY  😆");
